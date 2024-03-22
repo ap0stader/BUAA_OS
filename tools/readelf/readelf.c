c4fbc6fc99cdde4cc43b834f736976676ec788e0
贾博驿 <22371345@buaa.edu.cn>
2024-03-22 08:08:05 +0800

    Finish Exercise 1.1

--- tools/readelf/readelf.c
+++ tools/readelf/readelf.c
@@ -43,14 +43,18 @@ int readelf(const void *binary, size_t size) {
 	Elf32_Half sh_entry_count;
 	Elf32_Half sh_entry_size;
 	/* Exercise 1.1: Your code here. (1/2) */
-
+	// !!! 必须要转换为unsigned char，否则加一个整数是跳过一个Elf32_Ehdr
+	sh_table = (const unsigned char*)ehdr + ehdr->e_shoff;
+	sh_entry_count = ehdr->e_shnum;
+	sh_entry_size = ehdr->e_shentsize;
 	// For each section header, output its index and the section address.
 	// The index should start from 0.
 	for (int i = 0; i < sh_entry_count; i++) {
 		const Elf32_Shdr *shdr;
 		unsigned int addr;
 		/* Exercise 1.1: Your code here. (2/2) */
-
+		shdr = (Elf32_Shdr *)sh_table + i;
+		addr = shdr->sh_addr; 
 		printf("%d:0x%x\n", i, addr);
 	}
 
