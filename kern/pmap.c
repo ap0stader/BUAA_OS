920e0c8695c3832f3352837e1a7381b83204124f
贾博驿 <22371345@buaa.edu.cn>
2024-04-08 16:34:02 +0800

    Finish exercise 2.3

--- kern/pmap.c
+++ kern/pmap.c
@@ -93,16 +93,22 @@ void page_init(void) {
 	/* Step 1: Initialize page_free_list. */
 	/* Hint: Use macro `LIST_INIT` defined in include/queue.h. */
 	/* Exercise 2.3: Your code here. (1/4) */
-
+	LIST_INIT(page_free_list);
 	/* Step 2: Align `freemem` up to multiple of PAGE_SIZE. */
 	/* Exercise 2.3: Your code here. (2/4) */
-
+	freemem = ROUND(freemem, PAGE_SIZE);
 	/* Step 3: Mark all memory below `freemem` as used (set `pp_ref` to 1) */
 	/* Exercise 2.3: Your code here. (3/4) */
-
+	int used_page = page2pa(freemem) / PAGE_SIZE;
+	for (int i = 0; i < used_page; i++) {
+		pages[i].pp_ref = 1;
+	}
 	/* Step 4: Mark the other memory as free. */
 	/* Exercise 2.3: Your code here. (4/4) */
-
+	for (int i = used_page; i < npage; i++) {
+		pages[i].pp_ref = 0;
+		LIST_INSERT_HEAD(&page_free_list, &pages[i], pp_link);
+	}
 }
 
 /* Overview:
