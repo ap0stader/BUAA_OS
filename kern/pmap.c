bc61141f11b5aa0808549ac400481bc16f4c4801
贾博驿 <22371345@buaa.edu.cn>
2024-04-08 17:57:45 +0800

    Finish exercise 2.4 2.5 and fix exercise 2.3

--- kern/pmap.c
+++ kern/pmap.c
@@ -93,19 +93,19 @@ void page_init(void) {
 	/* Step 1: Initialize page_free_list. */
 	/* Hint: Use macro `LIST_INIT` defined in include/queue.h. */
 	/* Exercise 2.3: Your code here. (1/4) */
-	LIST_INIT(page_free_list);
+	LIST_INIT(&page_free_list);
 	/* Step 2: Align `freemem` up to multiple of PAGE_SIZE. */
 	/* Exercise 2.3: Your code here. (2/4) */
 	freemem = ROUND(freemem, PAGE_SIZE);
 	/* Step 3: Mark all memory below `freemem` as used (set `pp_ref` to 1) */
 	/* Exercise 2.3: Your code here. (3/4) */
-	int used_page = page2pa(freemem) / PAGE_SIZE;
-	for (int i = 0; i < used_page; i++) {
+	u_long marked_used_page = PPN(PADDR(freemem));
+	for (u_long i = 0; i < marked_used_page; i++) {
 		pages[i].pp_ref = 1;
 	}
 	/* Step 4: Mark the other memory as free. */
 	/* Exercise 2.3: Your code here. (4/4) */
-	for (int i = used_page; i < npage; i++) {
+	for (u_long i = marked_used_page; i < npage; i++) {
 		pages[i].pp_ref = 0;
 		LIST_INSERT_HEAD(&page_free_list, &pages[i], pp_link);
 	}
@@ -128,13 +128,15 @@ int page_alloc(struct Page **new) {
 	/* Step 1: Get a page from free memory. If fails, return the error code.*/
 	struct Page *pp;
 	/* Exercise 2.4: Your code here. (1/2) */
-
+	if (LIST_EMPTY(&page_free_list)) {
+		return -E_NO_MEM;
+	}
+	pp = LIST_FIRST(&page_free_list);
 	LIST_REMOVE(pp, pp_link);
-
 	/* Step 2: Initialize this page with zero.
 	 * Hint: use `memset`. */
 	/* Exercise 2.4: Your code here. (2/2) */
-
+	memset((void *)page2kva(pp), 0, PAGE_SIZE);
 	*new = pp;
 	return 0;
 }
@@ -149,7 +151,7 @@ void page_free(struct Page *pp) {
 	assert(pp->pp_ref == 0);
 	/* Just insert it into 'page_free_list'. */
 	/* Exercise 2.5: Your code here. */
-
+	LIST_INSERT_HEAD(&page_free_list, pp, pp_link);
 }
 
 /* Overview:
