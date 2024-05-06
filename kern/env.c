af7d2bf05e10ab0cbfa993964f271e782b040502
贾博驿 <22371345@buaa.edu.cn>
2024-05-06 15:19:37 +0800

    Finish exercise 4.3

--- kern/env.c
+++ kern/env.c
@@ -114,7 +114,12 @@ int envid2env(u_int envid, struct Env **penv, int checkperm) {
 	 *   You may want to use 'ENVX'.
 	 */
 	/* Exercise 4.3: Your code here. (1/2) */
-
+	if (envid == 0) {
+		e = curenv;
+		return 0;
+	} else {
+		e = &envs[ENVX(envid)];
+	}
 	if (e->env_status == ENV_FREE || e->env_id != envid) {
 		return -E_BAD_ENV;
 	}
@@ -126,7 +131,9 @@ int envid2env(u_int envid, struct Env **penv, int checkperm) {
 	 *   If violated, return '-E_BAD_ENV'.
 	 */
 	/* Exercise 4.3: Your code here. (2/2) */
-
+	if (checkperm && e->env_id != curenv->env_id && e->env_parent_id != curenv->env_id) {
+		return -E_BAD_ENV;
+	}
 	/* Step 3: Assign 'e' to '*penv'. */
 	*penv = e;
 	return 0;
