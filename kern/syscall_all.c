658b7bdae24bd01873fdf3f3013d0440bae36e3c
贾博驿 <22371345@buaa.edu.cn>
2024-05-06 16:52:38 +0800

    Finish exercise 4.7 and fix exercise 4.6

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -59,7 +59,7 @@ u_int sys_getenvid(void) {
 void __attribute__((noreturn)) sys_yield(void) {
 	// Hint: Just use 'schedule' with 'yield' set.
 	/* Exercise 4.7: Your code here. */
-
+	schedule(1);
 }
 
 /* Overview:
@@ -214,7 +214,7 @@ int sys_mem_unmap(u_int envid, u_int va) {
 	}
 	/* Step 2: Convert the envid to its corresponding 'struct Env *' using 'envid2env'. */
 	/* Exercise 4.6: Your code here. (2/2) */
-	try(envid2env(envid, &e, 1);
+	try(envid2env(envid, &e, 1));
 	/* Step 3: Unmap the physical page at 'va' in the address space of 'envid'. */
 	page_remove(e->env_pgdir, e->env_asid, va);
 	return 0;
