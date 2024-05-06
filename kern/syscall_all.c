7fbb182857a113b9ec4bf2449717f9c36fb9c796
贾博驿 <22371345@buaa.edu.cn>
2024-05-06 16:39:50 +0800

    Finish exercise 4.4

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -139,14 +139,16 @@ int sys_mem_alloc(u_int envid, u_int va, u_int perm) {
 
 	/* Step 1: Check if 'va' is a legal user virtual address using 'is_illegal_va'. */
 	/* Exercise 4.4: Your code here. (1/3) */
-
+	if (is_illegal_va(va)) {
+		return -E_INVAL;
+	}
 	/* Step 2: Convert the envid to its corresponding 'struct Env *' using 'envid2env'. */
 	/* Hint: **Always** validate the permission in syscalls! */
 	/* Exercise 4.4: Your code here. (2/3) */
-
+	try(envid2env(envid, &env, 1));
 	/* Step 3: Allocate a physical page using 'page_alloc'. */
 	/* Exercise 4.4: Your code here. (3/3) */
-
+	try(page_alloc(&pp));
 	/* Step 4: Map the allocated page at 'va' with permission 'perm' using 'page_insert'. */
 	return page_insert(env->env_pgdir, env->env_asid, pp, va, perm);
 }
