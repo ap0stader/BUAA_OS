0c1bd1b4ecf95a4ccbd958d4700ae35abf4a11e0
贾博驿 <22371345@buaa.edu.cn>
2024-06-25 20:50:00 +0800

    A very crazy test

--- include/sigaction.h
+++ include/sigaction.h
@@ -56,7 +56,7 @@ struct sigaction {
 // 是否是合法的信号
 #define is_legal_signo(__signo) (MINSIGNO <= (__signo) && (__signo) <= MAXSIGNO)
 // 生成信号对应的掩码
-#define signo2mask(__signo) ((uint32_t)(1 << ((__signo) - 1)))
+#define signo2mask(__signo) ((uint32_t)(1 << ((__signo))))
 
 // 修改进程的屏蔽的方法
 enum {
