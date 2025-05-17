85689d8d27f9460cc7349df37417c890bb54e2cb
贾博驿 <22371345@buaa.edu.cn>
2025-05-17 20:12:38 +0800

    Update tool

--- tests/lab5_encryptfs_tool/encrypt_tool.c
+++ tests/lab5_encryptfs_tool/encrypt_tool.c
@@ -1,8 +1,10 @@
 #include <lib.h>
 
-static char *msg = "Let the ruling classes tremble at a Communistic revolution.\nThe proletarians have nothing to lose but their chains.\nThey have a world to win.\n";
+// static char *msg = "Let the ruling classes tremble at a Communistic revolution.\nThe proletarians have nothing to lose but their chains.\nThey have a world to win.\n";
 
 // static char *msg = "Workers of the world, unite!";
+
+static char *msg = "A spectre is haunting Europe - the spectre of Communism.\nAll the Powers of old Europe have entered into a holy alliance to exorcize this spectre:\nPope and Czar, Metternich and Guizot, French Radicals and German police spies.";
 
 int main() {
     int r;
@@ -10,21 +12,30 @@ int main() {
     char buf[512];
     
     key_fd = open("/key0.key", O_RDONLY);
-    fskey_set(key_fd);
+    r = fskey_set(key_fd);
+    debugf("%d\n", r);
     close(key_fd);
 
     msg_fd = open("/newmsg", O_RDWR | O_ENCRYPT | O_CREAT);
-    write(msg_fd, msg, strlen(msg) + 1);
-    close(msg_fd);
+    r = write(msg_fd, msg, strlen(msg) + 1);
+    debugf("%d\n", r);
+    r = close(msg_fd);
+    debugf("%d\n", r);
 
     msg_fd = open("/newmsg", O_RDONLY);
     read(msg_fd, buf, 511);
     for (int i = 0; i < strlen(msg) + 1; i++) {
-        debugf(" %c ", msg[i]);
+        debugf(" %c", msg[i]);
+        if (msg[i] != '\n') {
+            debugf(" ");
+	}
     }
     debugf("\n");
     for (int i = 0; i < strlen(msg) + 1; i++) {
         debugf("%02x ", (unsigned char) buf[i]);
+        if (msg[i] == '\n') {
+            debugf("\n");
+        }
     }
     debugf("\n");
     close(msg_fd);
