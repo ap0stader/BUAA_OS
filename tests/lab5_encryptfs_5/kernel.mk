bfd43c5265a6ce362c7e7413ae4f2d919da734d8
ap0stader <ap0stader@outlook.com>
2025-05-18 01:52:36 +0800

    Add judge 5

---
+++ tests/lab5_encryptfs_5/kernel.mk
@@ -0,0 +1,2 @@
+init-envs += master /fs_serv slave
+fs-files  += $(wildcard $(test_dir)/rootfs/*)
---
+++ tests/lab5_encryptfs_5/master.c
@@ -0,0 +1,37 @@
+#include <lib.h>
+
+int main() {
+	int r;
+	int key_fd;
+
+	// Invalid key
+	if ((r = open("/invalid_magic.key", O_RDONLY)) < 0) {
+		user_panic("[MASTER] open(invalid_magic.key) failed: %d\n", r);
+	}
+	key_fd = r;
+	if ((r = fskey_set(key_fd)) != -E_INVALID_KEY_FILE) {
+		user_panic("[MASTER] fskey_set(invalid_magic.key) failed: %d\n", r);
+	}
+	if ((r = close(key_fd)) != 0) {
+		user_panic("[MASTER] close(invalid_magic.key) failed: %d\n", r);
+	}
+	debugf("[MASTER] fskey_set(invalid_magic.key) passed\n");
+
+	// Set key as key1.key (Valid key)
+	if ((r = open("/key1.key", O_RDONLY)) < 0) {
+		user_panic("[MASTER] open(key1.key) failed: %d\n", r);
+	}
+	key_fd = r;
+	if ((r = fskey_set(key_fd)) != 0) {
+		user_panic("[MASTER] fskey_set(key1.key) failed: %d\n", r);
+	}
+	if ((r = fskey_isset()) != 1) {
+		user_panic("[MASTER] fskey_isset(key1.key) failed: %d\n", r);
+	}
+	if ((r = close(key_fd)) != 0) {
+		user_panic("[MASTER] close(key1.key) failed: %d\n", r);
+	}
+	debugf("\n[MASTER] fskey_set(key1.key) passed\n");
+
+	return 0;
+}
